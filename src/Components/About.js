2a017b14e99606dc6b865f525eb3570c22bfeaa3
sunilkumar35 <rkt.sunilkumar97@gmail.com>
2021-01-15 19:29:41 +0530

    make it batter

---
+++ src/Components/About.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+
+    if(this.props.data){
+      var name = this.props.data.name;
+      var profilepic= "https://avatars3.githubusercontent.com/u/46319649?s=460&u=7fb6420b7112c34f17e9d82b868fce9ec1111b85&v=4";
+      var bio = this.props.data.bio;
+      var street = this.props.data.address.street;
+      var city = this.props.data.address.city;
+      var state = this.props.data.address.state;
+      var zip = this.props.data.address.zip;
+      var phone= this.props.data.phone;
+      var email = this.props.data.email;
+      var resumeDownload = this.props.data.resumedownload;
+    }
+
+    return (
+      <section id="about">
+      <div className="row">
+         <div className="three columns">
+            <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
+         </div>
+         <div className="nine columns main-col">
+            <h2>About Me</h2>
+
+            <p>{bio}</p>
+            <div className="row">
+               <div className="columns contact-details">
+                  <h2>Contact Details</h2>
+                  <p className="address">
+						   <span>{name}</span><br />
+						   <span>{street}<br />
+						         {city} {state}, {zip}
+                   </span><br />
+						   <span>{phone}</span><br />
+                     <span>{email}</span>
+					   </p>
+               </div>
+               <div className="columns download">
+                  <p>
+                     <a href='https://drive.google.com/file/d/1Muq_ulVYyhMiOdFPUeA5pCaDWfM05gOg/view?usp=sharing' className="button"><i className="fa fa-download"></i>Download Resume</a>
+                  </p>
+               </div>
+               
+            </div>
+         </div>
+      </div>
+
+   </section>
+    );
+  }
+}
+
+export default About;
